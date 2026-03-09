2703f17392efea12f1ffdc23bcc0cd9a1911b995
rhey <rheychristian26@gmail.com>
2026-03-09 17:34:18 +0800

    whatever

--- public/server.php
+++ public/server.php
@@ -3,10 +3,30 @@
 $uri = urldecode(parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH));
 $filePath = __DIR__ . $uri;
 
-error_log("URI: $uri | Looking for: $filePath | Exists: " . (file_exists($filePath) ? 'YES' : 'NO'));
-
 if ($uri !== '/' && file_exists($filePath) && is_file($filePath)) {
-    return false;
+    $ext = pathinfo($filePath, PATHINFO_EXTENSION);
+    $mimeTypes = [
+        'css'   => 'text/css; charset=utf-8',
+        'js'    => 'application/javascript; charset=utf-8',
+        'png'   => 'image/png',
+        'jpg'   => 'image/jpeg',
+        'jpeg'  => 'image/jpeg',
+        'gif'   => 'image/gif',
+        'svg'   => 'image/svg+xml',
+        'ico'   => 'image/x-icon',
+        'woff'  => 'font/woff',
+        'woff2' => 'font/woff2',
+        'json'  => 'application/json',
+        'webp'  => 'image/webp',
+    ];
+
+    if (isset($mimeTypes[$ext])) {
+        header('Content-Type: ' . $mimeTypes[$ext]);
+    }
+
+    header('Content-Length: ' . filesize($filePath));
+    readfile($filePath);
+    exit;
 }
 
 require_once __DIR__ . '/index.php';
