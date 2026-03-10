34718b26c9f93907980c92a0b0ea9d287b481495
rhey <rheychristian26@gmail.com>
2026-03-10 13:20:15 +0800

    add missing books resource route

--- routes/web.php
+++ routes/web.php
@@ -49,6 +49,7 @@ Route::middleware(['auth', 'role:admin'])->prefix('admin')->name('admin.')->grou
     
     // Resource route AFTER custom routes
     Route::resource('users', App\Http\Controllers\Admin\UserController::class);
+    Route::resource('books', App\Http\Controllers\Admin\BookController::class); // ADD THIS
     
     Route::post('/users/{user}/toggle', [App\Http\Controllers\Admin\UserController::class, 'toggle'])->name('users.toggle');
     Route::post('/users/{user}/resend', [App\Http\Controllers\Admin\UserController::class, 'resendSetPassword'])->name('users.resend');
